adfa1bcaab9f23aacd81b1861e12a65d8f94779f
Niki3 <franczyslaw09@gmail.com>
2023-12-23 20:33:10 +0100

    Added comments

--- src/main.c
+++ src/main.c
@@ -6,13 +6,17 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#define DEFAULT_PROGRAM_NAME "exe"
+
 int main(int argc, char* argv[]) {
 
-  char program_name[100] = "exe";
+  //Checks if the first argument is passed in, and sets the program_name variable to it. If not, it defalts to DEFAULT_PROGRAM_NAME
+  char program_name[100] = DEFAULT_PROGRAM_NAME;
   if (argv[1] != NULL) {
     strcpy(program_name, argv[1]);
   }
   
+  //Creates the src directory if it doesn't exist already
   struct stat st = {0};
   if(stat("src", &st) == -1) {
     mkdir("src", 00700);
@@ -21,7 +25,10 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
+  //Default string for main.c
   char base_main[] = "#include <stdio.h>\n\nint main() {\n   printf(\"Hello world!\\n\");\n}\n";
+
+  //This whole bit sets the Makefile source, concatanating the program_name where it needs to
   char bit1[50] = "none:\n\tgcc src/*.c -o ";
   char bit2[50] = "\nrun:\n\tgcc src/*.c -o ";
   char bit3[50] = "\n\t./";
@@ -36,27 +43,30 @@ int main(int argc, char* argv[]) {
   strcat(base_makefile, bit4);
   
 
+  //Checks if the main.c file exists and opens it
   FILE* file = fopen("src/main.c", "w");
-
   if (file == NULL) {
     printf("Could not open the \"main.c\" file\n");
     return 1;
   }
 
+  //Writes the default source code for the main.c file into the file
   if (fprintf(file, base_main) < 0) {
     printf("Could not write to the main.c file\n");
     return 1;
   }
 
+  //Closes the file
   fclose(file);
 
+  //Checks and opens the Makefile
   file = fopen("Makefile", "w");
-
   if (file == NULL) {
     printf("Could not open the \"Makefile\" file\n");
     return 1;
   }
 
+  //Writes the source code into the Makefile
   if (fprintf(file, base_makefile) < 0) {
     printf("Could not write to the \"Makefile\" file\n");
   }
